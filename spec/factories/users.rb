c8001e588af159050f1067591193b0aee055af45
David Parry <david.parry@suranyami.com>
2015-05-18 10:48:21 +1000

    added first_name, last_name to user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,14 @@
 FactoryGirl.define do
   factory :user do
     confirmed_at Time.now
-    name "Test User"
-    email "test@example.com"
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    email Faker::Internet.email
     password "please123"
+    password_confirmation "please123"
 
     trait :admin do
       role 'admin'
     end
-
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,26 @@
 describe User do
 
-  before(:each) { @user = User.new(email: 'user@example.com') }
+  let(:user) {User.new(email: 'user@example.com')}
 
-  subject { @user }
+  subject { user }
 
-  it { should respond_to(:email) }
+  it {should respond_to(:email)}
 
   it "#email returns a string" do
-    expect(@user.email).to match 'user@example.com'
+    expect(user.email).to match 'user@example.com'
   end
 
+  it {expect(user).to respond_to(:name_or_email)}
+
+  context 'with no name' do
+    let(:user) {
+      FactoryGirl.create(:user,
+        email: 'user@example.com',
+        first_name: '',
+        last_name: '')
+    }
+    it 'returns email instead' do
+      expect(user.email).to eql('user@example.com')
+    end
+  end
 end
